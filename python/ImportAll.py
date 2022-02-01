651fbac96e466e125a1c68fee3cfec69d6b5bb91
Arnaud Muller <arnaud.muller1308@gmail.com>
2022-02-01 20:19:01 +0100

    Finished import code in python

---
+++ python/ImportAll.py
@@ -0,0 +1,56 @@
+# Preamble
+import json
+import requests
+import math
+
+# Import parameters
+API = 'll'  # ll or lldev
+API_version = '2.2.0'
+with open('APIkey.txt', 'r') as f:
+    API_key = f.read()
+    f.close()
+header = {'Authorization': 'Token ' + API_key}
+
+
+def ll2_call(data_name, endpoint, call_headers, api, api_version):
+    next_url = 'https://' + api + '.thespacedevs.com/' + api_version + '/' + endpoint + '/?limit=100&mode=detailed'
+    ii = 0
+    number_calls = 0
+    data = []
+    while next_url is not None:
+        ii += 1
+        call = requests.get(next_url, headers=call_headers, timeout=20).json()
+        if ii == 1:
+            number_data = call['count']
+            print('Total ' + data_name + ' : ' + str(number_data))
+            number_calls = math.ceil(number_data / 100.0)
+        data.append(call['results'])
+        next_url = call['next']
+        print('API Call ' + str(ii) + '/' + str(number_calls))
+    return data
+
+
+def export(data_name, path, data):
+    with open(path, 'w+') as file:
+        json.dump(data, file, indent=4)
+        file.close()
+    print('Successfully exported ' + data_name + ' to file.')
+
+
+# Import Launches
+export('Launches', 'data/Launches.json', ll2_call('Launches', 'launch', header, API, API_version))
+
+# Import Orbits
+export('Orbits', 'data/Orbits.json', ll2_call('Orbits', 'config/orbit', header, API, API_version))
+
+# Import Statuses
+export('Statuses', 'data/Statuses.json', ll2_call('Statuses', 'config/launchstatus', header, API, API_version))
+
+# Import Agencies
+export('Agencies', 'data/Agencies.json', ll2_call('Agencies', 'agencies', header, API, API_version))
+
+# Import Locations
+export('Locations', 'data/Locations.json', ll2_call('Locations', 'location', header, API, API_version))
+
+# Import Pads
+export('Pads', 'data/Pads.json', ll2_call('Pads', 'pad', header, API, API_version))
