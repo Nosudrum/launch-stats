c5bb903be6670f3bb4c2c8bd42d141fbdedb1153
Arnaud Muller <arnaud.muller1308@gmail.com>
2022-02-02 18:20:53 +0100

    Lots of progress on python

--- python/Processing.py
+++ python/Processing.py
@@ -1,7 +1,9 @@
 # Preamble
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Import json data
 def import_json(path):
@@ -62,10 +64,95 @@ for Location in Locations:
         LocsLat.append(None)
         LocsLong.append(None)
 
+
 # Figures
+def dark_figure():
+    fig = plt.figure(facecolor='black', figsize=(7, 5))
+    f = plt.gca()
+    for i in f.spines:
+        f.spines[i].set_color('white')
+    f.tick_params(axis='x', colors='white', which='both')
+    f.tick_params(axis='y', colors='white', which='both')
+    f.yaxis.label.set_color('white')
+    f.xaxis.label.set_color('white')
+    f.title.set_color('white')
+    f.set_facecolor('black')
+    return f
+
+
 # figure
 # geobubble(LocsLat,LocsLong,LocsLaunchCount,'BubbleColorList',[1 0 0])
 # geobasemap('satellite')
+
+
+plt.figure()
+F1_Years = [i.year for i in LaunchT0 if i < datetime.now(timezone.utc).replace(tzinfo=None)]
+
+plt.hist(F1_Years, bins=np.unique(F1_Years))
+plt.xlabel('Year')
+plt.ylabel('# of launches')
+plt.ylim([0, 170])
+plt.title('All launches attempts')
+plt.savefig('plots/test1.png', transparent=True, dpi=300)
+plt.savefig('plots/test1.pdf', transparent=True, dpi=300)
+plt.show()
+
+plt.figure()
+F2_Years = [i[1].year for i in enumerate(LaunchT0) if
+            i[1] < datetime.now(timezone.utc).replace(tzinfo=None) and LaunchOrbit[i[0]] != 15]
+plt.hist(F2_Years, bins=np.unique(F2_Years), alpha=0.5)
+plt.xlabel('Year')
+plt.ylabel('# of launches')
+plt.ylim([0, 170])
+plt.title('Orbital attempts')
+plt.savefig('plots/test2.png', transparent=True, dpi=300)
+plt.savefig('plots/test2.pdf', transparent=True, dpi=300)
+plt.show()
+
+F4_Countries = ['USA', ['RUS', 'KAZ'], ['FRA', 'GUF'], 'CHN', 'JPN', 'IND', 'NZL']
+F4_Countries_Flatten = []
+for i in F4_Countries:
+    if isinstance(i, list):
+        F4_Countries_Flatten += i
+    else:
+        F4_Countries_Flatten.append(i)
+F4_Countries_Labels = ['USA', 'Russia/USSR', 'France', 'China', 'Japan', 'India', 'New Zealand', 'Others']
+F4_Years = [i.year for i in LaunchT0 if i < datetime.now(timezone.utc).replace(tzinfo=None)]
+F4 = dark_figure()
+F4_data = []
+for ii in F4_Countries:
+    if isinstance(ii, list):
+        tmp = []
+        for jj in ii:
+            tmp += [i[1].year for i in enumerate(LaunchT0) if
+                    i[1] < datetime.now(timezone.utc).replace(tzinfo=None) and LaunchOrbit[i[0]] != 15 and
+                    LaunchCountry[
+                        i[0]] == jj]
+        F4_data.append(tmp)
+    else:
+        F4_data.append([i[1].year for i in enumerate(LaunchT0) if
+                        i[1] < datetime.now(timezone.utc).replace(tzinfo=None) and LaunchOrbit[i[0]] != 15 and
+                        LaunchCountry[
+                            i[0]] == ii])
+F4_data.append([i[1].year for i in enumerate(LaunchT0) if
+                i[1] < datetime.now(timezone.utc).replace(tzinfo=None) and LaunchOrbit[i[0]] != 15 and
+                LaunchCountry[
+                    i[0]] not in F4_Countries_Flatten])
+plt.hist(F4_data, bins=np.unique(F4_Years), histtype='bar', stacked=True)
+
+plt.legend(F4_Countries_Labels, loc='upper center', ncol=4, frameon=False, labelcolor='white')
+# legend = plt.legend(frameon=1)
+# frame = legend.get_frame()
+# frame.set_facecolor('black')
+# frame.set_edgecolor('white')
+plt.xlabel('Year')
+plt.ylabel('# of launches')
+plt.ylim([0, 180])
+plt.xlim([min(F4_Years), max(F4_Years)])
+plt.title('Orbital attempts by country since ' + str(min(F4_Years)))
+plt.savefig('plots/OrbitalAttemptsPerCountryStacked.png', transparent=True, dpi=300)
+plt.savefig('plots/OrbitalAttemptsPerCountryStacked.pdf', transparent=True, dpi=300)
+plt.show()
 
 # plt.figure()
 # plt.show()
@@ -79,10 +166,3 @@ for Location in Locations:
 # ylim([0 150])
 # exportgraphics(gcf,'plots/OrbitalAttemptsPerCountry.pdf')
 # exportgraphics(gcf,'plots/OrbitalAttemptsPerCountry.png','Resolution',300)
-
-
-
-
-
-
-
