8ae71c7d85d46c3a17cd15b2ad99ff46dec60778
Arnaud Muller <arnaud.muller1308@gmail.com>
2022-03-05 15:58:55 +0100

    Added plots per LSP

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Nosu's LL2 Launch Stats
-Repo of Nosu's launch statistics using the Launch Library 2 API.
+In this repository you will find various plots related to space launches made using the <a href="https://thespacedevs.com/llapi">Launch Library 2 API</a> and scripts written in [python scripts](python). The [MATLAB](matlab) scripts are no longer used, but are still visible if anyone is interested.
 
-[Yearly plots](python/plots/yearly/orbitalAttemptsPerCountry/README.md)
+If you have feedback or ideas, feel free to open an issue 🙂
+
+## Yearly plots
+
+### All yearly plots since 1957
+
+🔗 [Yearly plots per country](python/plots/yearly/orbitalAttemptsPerCountry/README.md)
+
+🔗 [Yearly plots per LSP](python/plots/yearly/orbitalAttemptsPerLSP/README.md)
+
+### 2022 plots
+
+![Orbital attempts per country in 2022](python/plots/yearly/orbitalAttemptsPerCountry/2022_transparent.png)
+
+![Orbital attempts per LSP in 2022](python/plots/yearly/orbitalAttemptsPerLSP/2022_transparent.png)
+
+## Historical data plots
 
 ![Orbital attempts per country stacked](python/plots/OrbitalAttemptsPerCountryStacked_transparent.png)
 
 ![Orbital attempts per country](python/plots/OrbitalAttemptsPerCountry_transparent.png)
 
-![Orbital attempts per country in 2022](python/plots/yearly/orbitalAttemptsPerCountry/2022_transparent.png)
--- python/plots.py
+++ python/plots.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cartopy.crs as ccrs
 import calendar
-from Processing import LaunchT0, LaunchOrbit, LaunchCountry, LocsName, LocsLaunchCount, LocsLat, LocsLon
+from Processing import LaunchT0, LaunchOrbit, LaunchCountry, LocsName, LocsLaunchCount, LocsLat, LocsLon, LaunchLSP, \
+    Agencies
 from PIL import Image
 
 Badge_DataLL2 = Image.open('assets/DataByLL2.png')
@@ -78,14 +79,22 @@ plt.xlabel(datetime.now(timezone.utc).strftime("Plot generated on %Y/%m/%d at %H
            labelpad=10)
 # finish_figure(F0, 'plots/test1.png')
 
+colors_dict = {'red': '#e41a1c', 'orange': '#ff7f00', 'blue': '#377eb8', 'pink': '#f781bf', 'yellow': '#dede00',
+               'green': '#4daf4a', 'grey': '#999999', 'purple': '#984ea3'}
+colors = ['blue', 'orange', 'red', 'green', 'pink', 'yellow', 'purple', 'grey']
+colors = [colors_dict[i] for i in colors]
+LSPs_dict = {66: 'Soviet Union', 161: 'USAF', 63: 'ROSCOSMOS', 88: 'CASC', 115: 'Arianespace',
+             96: 'Khrunichev ', 121: 'SpaceX', 147: 'Rocket Lab', 124: 'ULA', 257: 'NGSS', 285: 'Astra',
+             199: 'Virgin Orbit', 163: 'VVKO', 166: 'US Navy', 271: 'ABMA', 165: 'US Army', 44: 'NASA', 270: 'RVSN RF',
+             1018: 'CNR IT', 1017: 'SERC UK', 46: 'CNES FR', 1009: 'ISAS JP', 1019: 'ESRO', 1015: 'ELDO',
+             1005: 'RAE UK', 189: 'CASC', 82: 'Lockheed M.', 228: 'NASDA', 100: 'OSC', 1004: 'Convair', 102: 'Rockwell',
+             192: 'Lockheed SOC', 98: 'Mitsubishi HI', 1014: 'Martin M.', 111: 'Progress RSC', 154: 'Polyot',
+             197: 'LMSO', 191: 'USA', 122: 'Sea Launch', 37: 'JAXA', 118: 'ILS', 193: 'VKS', 31: 'ISRO', 194: 'ExPace'}
+
 # Plot of orbital launch attempts per country since 1957 stacked
 F1_Countries = [['RUS', 'KAZ'], 'USA', 'CHN', ['FRA', 'GUF'], 'JPN', 'IND', 'NZL']
 F1_Countries_Flatten = flatten(F1_Countries)
 F1_Countries_Labels = ['Russia/USSR', 'USA', 'China', 'France', 'Japan', 'India', 'New Zealand', 'Others']
-colors = {'red': '#e41a1c', 'orange': '#ff7f00', 'blue': '#377eb8', 'pink': '#f781bf', 'yellow': '#dede00',
-          'green': '#4daf4a', 'grey': '#999999', 'purple': '#984ea3'}
-F1_colors = ['blue', 'orange', 'red', 'green', 'pink', 'yellow', 'purple', 'grey']
-F1_colors = [colors[i] for i in F1_colors]
 F1_Years = [i.year for i in LaunchT0 if i < datetime.now(timezone.utc).replace(tzinfo=None)]
 _, F1 = dark_figure()
 F1_data = []
@@ -108,7 +117,7 @@ F1_data.append([i[1].year for i in enumerate(LaunchT0) if
                 LaunchCountry[
                     i[0]] not in F1_Countries_Flatten])
 plt.hist(F1_data, bins=np.append(np.unique(F1_Years), max(F1_Years) + 1), histtype='bar', stacked=True,
-         label=F1_Countries_Labels, color=F1_colors)
+         label=F1_Countries_Labels, color=colors)
 handles, labels = flip_legend(reverse=False)
 plt.legend(handles, labels, loc='upper center', ncol=4, frameon=False, labelcolor='white')
 plt.ylabel('Total launches per year')
@@ -122,7 +131,7 @@ finish_figure(F1, 'OrbitalAttemptsPerCountryStacked', show=True)
 # Plot of orbital launch attempts per country since 1957 non-stacked
 _, F2 = dark_figure()
 plt.hist(F1_data, bins=np.append(np.unique(F1_Years), max(F1_Years) + 1), histtype='step', stacked=False,
-         label=F1_Countries_Labels, color=F1_colors, linewidth=2)
+         label=F1_Countries_Labels, color=colors, linewidth=2)
 handles, labels = flip_legend(reverse=True)
 plt.legend(handles, labels, loc='upper center', ncol=4, frameon=False, labelcolor='white')
 plt.ylabel('Launches per year')
@@ -166,7 +175,7 @@ for year in range(1957, datetime.now(timezone.utc).year + 1):
                      i[1].year == year and LaunchOrbit[i[0]] != 15 and LaunchCountry[
                          i[0]] not in F1_Countries_Flatten and i[1] < datetime.now(timezone.utc).replace(tzinfo=None)])
     plt.hist(F1y_data, bins=np.append(days, max(days) + 1), histtype='step', cumulative=True, stacked=False,
-             linewidth=2, label=F1_Countries_Labels, color=F1_colors)
+             linewidth=2, label=F1_Countries_Labels, color=colors)
     handles, labels = flip_legend(reverse=True)
     plt.legend(handles, labels, loc='upper center', ncol=4, frameon=False,
                labelcolor='white')
@@ -178,7 +187,83 @@ for year in range(1957, datetime.now(timezone.utc).year + 1):
                labelpad=10)
     plt.title('Orbital launch attempts per country in ' + str(year))
     finish_figure(F3, 'yearly/orbitalAttemptsPerCountry/' + str(year), show=False)
+F1y_README.close()
 print('Done with yearly launch plots by country')
 
-F1y_README.close()
+# Plot of orbital launch attempts per LSP since 1957 stacked
+F3_LSPs_Labels = ['SpaceX', 'ULA', 'CASC', 'Rocket Lab', 'ROSCOSMOS', 'Arianespace', 'Northrop Grumman', 'Others']
+F3_Years = [i.year for i in LaunchT0 if i < datetime.now(timezone.utc).replace(tzinfo=None)]
+_, F3 = dark_figure()
+F3_data = []
+F3_LSPs_ID = []
+for LSP in Agencies:
+    F3_LSP_ID = LSP['id']
+    F3_data_tmp = [i[1].year for i in enumerate(LaunchT0) if
+                   i[1] < datetime.now(timezone.utc).replace(tzinfo=None) and LaunchOrbit[i[0]] != 15 and
+                   LaunchLSP[i[0]] == F3_LSP_ID]
+    if F3_data_tmp:
+        F3_data.append(F3_data_tmp)
+        F3_LSPs_ID.append(LSP['id'])
+F3_data_len = [len(x) for x in F3_data]
+F3_data = [element for _, element in sorted(zip(F3_data_len, F3_data))]
+F3_data = F3_data[-7::][::-1] + [flatten(F3_data[0:-7])]
+F3_LSPs_ID = [element for _, element in sorted(zip(F3_data_len, F3_LSPs_ID))]
+F3_LSPs_ID = F3_LSPs_ID[-7::][::-1]
+F3_LSPs_Labels = [LSPs_dict[ii] for ii in F3_LSPs_ID] + ['Others']
+plt.hist(F3_data, bins=np.append(np.unique(F3_Years), max(F3_Years) + 1), histtype='bar', stacked=True,
+         label=F3_LSPs_Labels, color=colors)
+handles, labels = flip_legend(reverse=False)
+plt.legend(handles, labels, loc='upper center', ncol=4, frameon=False, labelcolor='white')
+plt.ylabel('Total launches per year')
+plt.ylim([0, 180])
+plt.xlim([min(F3_Years), max(F3_Years) + 1])
+plt.title('Orbital launch attempts per LSP since ' + str(min(F3_Years)))
+plt.xlabel(datetime.now(timezone.utc).strftime("Plot generated on %Y/%m/%d at %H:%M:%S UTC."), color='dimgray',
+           labelpad=10)
+finish_figure(F3, 'OrbitalAttemptsPerLSPStacked', show=True)
+
+# Plot of orbital launch attempts per LSP per year since 1957
+F3y_README = open('plots/yearly/orbitalAttemptsPerLSP/README.md', 'w')
+F3y_README.write('# Orbital attempts per LSP for every year since 1957\n')
+monthsLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+print('Starting yearly launch plots by LSP')
+for year in range(1957, datetime.now(timezone.utc).year + 1):
+    print(year)
+    F3y_README.write('![Orbital attempts per LSP in ' + str(year) + '](' + str(year) + '_transparent.png)\n')
+    days = list(range(1, 1 + (366 if calendar.isleap(year) else 365)))
+    if year > 1957:
+        del F3
+    _, F3 = dark_figure()
+    F3y_data = []
+    F3y_LSPs_ID = []
+    for LSP in Agencies:
+        F3y_LSP_ID = LSP['id']
+        F3y_data_tmp = [i[1].timetuple().tm_yday for i in enumerate(LaunchT0) if
+                        i[1].year == year and LaunchOrbit[i[0]] != 15 and LaunchLSP[i[0]] == F3y_LSP_ID and i[
+                            1] < datetime.now(timezone.utc).replace(tzinfo=None)]
+        if F3y_data_tmp:
+            F3y_data.append(F3y_data_tmp)
+            F3y_LSPs_ID.append(LSP['id'])
+    F3y_data_len = [len(x) for x in F3y_data]
+    F3y_data = [element for _, element in sorted(zip(F3y_data_len, F3y_data))]
+    F3y_data = F3y_data[-7::][::-1] + [flatten(F3y_data[0:-7])]
+    F3y_LSPs_ID = [element for _, element in sorted(zip(F3y_data_len, F3y_LSPs_ID))]
+    F3y_LSPs_ID = F3y_LSPs_ID[-7::][::-1]
+    F3y_LSPs_Labels = [LSPs_dict[ii] for ii in F3y_LSPs_ID] + ['Others']
+    plt.hist(F3y_data, bins=np.append(days, max(days) + 1), histtype='step', cumulative=True, stacked=False,
+             linewidth=2, label=F3y_LSPs_Labels, color=colors[0:len(F3y_data)])
+    handles, labels = flip_legend(reverse=True)
+    plt.legend(handles, labels, loc='upper center', ncol=4, frameon=False,
+               labelcolor='white')
+    plt.xticks([datetime(year, i, 1).timetuple().tm_yday for i in range(1, 13)], monthsLabels)
+    plt.ylabel('Cumulative number of launches')
+    plt.ylim([0, max([len(x) for x in F3y_data]) * 1.2])
+    plt.xlim([1, max(days)])
+    plt.xlabel(datetime.now(timezone.utc).strftime("Plot generated on %Y/%m/%d at %H:%M:%S UTC."), color='dimgray',
+               labelpad=10)
+    plt.title('Orbital launch attempts per LSP in ' + str(year))
+    finish_figure(F3, 'yearly/orbitalAttemptsPerLSP/' + str(year), show=False)
+print('Done with yearly launch plots by LSP')
+F3y_README.close()
+
 print('Successfully generated and exported all plots.')
