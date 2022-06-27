416bdf0f7b471b58b8e6648e18683d3db0f45419
Arnaud Muller <arnaud.muller1308@gmail.com>
2022-06-27 20:49:04 +0200

    Updated data

--- python/plots.py
+++ python/plots.py
@@ -3,25 +3,25 @@ import plotsCodes
 
 # Figures
 # Main plots
-# plotsCodes.OrbitalAttemptsPerCountryStacked.main(show=True)
-# plotsCodes.OrbitalAttemptsPerCountry.main(show=True)
-# plotsCodes.OrbitalAttemptsPerLSPStacked.main(show=True)
-# plotsCodes.OrbitalAttemptsPerLSP.main(show=True)
-# plotsCodes.OrbitalAttemptsPerLSPStacked.main(show=True)
-# plotsCodes.OrbitalAttemptsPerLSPType.main(show=True)
-# plotsCodes.OrbitalAttemptsPerLSPTypeStacked.main(show=True)
-# plotsCodes.DailyOrbitalAttemptsPerCountry.main(show=True)
-# plotsCodes.DailyOrbitalFailuresPerCountry.main(show=True)
-# plotsCodes.DailyOrbitalSuccessRate.main(show=True)
+plotsCodes.OrbitalAttemptsPerCountryStacked.main(show=True)
+plotsCodes.OrbitalAttemptsPerCountry.main(show=True)
+plotsCodes.OrbitalAttemptsPerLSPStacked.main(show=True)
+plotsCodes.OrbitalAttemptsPerLSP.main(show=True)
+plotsCodes.OrbitalAttemptsPerLSPStacked.main(show=True)
+plotsCodes.OrbitalAttemptsPerLSPType.main(show=True)
+plotsCodes.OrbitalAttemptsPerLSPTypeStacked.main(show=True)
+plotsCodes.DailyOrbitalAttemptsPerCountry.main(show=True)
+plotsCodes.DailyOrbitalFailuresPerCountry.main(show=True)
+plotsCodes.DailyOrbitalSuccessRate.main(show=True)
 # Plots per Country
-# plotsCodes.LaunchCadenceByCountry.main(show=False)
+plotsCodes.LaunchCadenceByCountry.main(show=False)
 
 # Plots per LSP
-# plotsCodes.LaunchCadenceByLSP.main(show=False)
+plotsCodes.LaunchCadenceByLSP.main(show=False)
 
 # Yearly plots
-# plotsCodes.YearlyOrbitalAttemptsPerCountry.main(show=False)
-# plotsCodes.YearlyOrbitalAttemptsPerLSP.main(show=False)
+plotsCodes.YearlyOrbitalAttemptsPerCountry.main(show=False)
+plotsCodes.YearlyOrbitalAttemptsPerLSP.main(show=False)
 
 # Plotly plots
 plotsCodes.PlotlyOrbitalAttemptsPerCountry.main()
