6bf403b73cecc1247885fbc2201b976bccfbd44f
Arnaud Muller <arnaud.muller1308@gmail.com>
2022-11-16 10:11:48 +0100

    Updated data & fixed maiden flights plot

--- python/plotsCodes/MaidenFlights.py
+++ python/plotsCodes/MaidenFlights.py
@@ -1,4 +1,4 @@
-from Processing import PastRocket
+from Processing import PastRocket, PastT0s
 from plotsCodes.PlotFunctions import flip_legend, finish_figure, np, dark_figure, colors, datetime, timezone
 
 
@@ -6,21 +6,20 @@ from plotsCodes.PlotFunctions import flip_legend, finish_figure, np, dark_figure
 def main(show=True):
     print('Starting maiden flights plot since 1957')
     data = PastRocket[
-        ["configuration.name", "configuration.family", "configuration.maiden_flight"]].copy().rename(
+        ["configuration.name", "configuration.family", "configuration.id"]].copy().rename(
         columns={"configuration.name": "name", "configuration.family": "family",
-                 "configuration.maiden_flight": "mf"}).drop_duplicates()
+                 "configuration.id": "id"}).drop_duplicates()
     current_year = datetime.now(timezone.utc).year
     families_found = []
     maiden_flights_newFamily = []
     maiden_flights_oldFamily = []
     for _, row in data.iterrows():
-        if row["mf"] is not None:
-            mf_year = datetime.strptime(row["mf"], "%Y-%m-%d").year
-            if row["family"] in families_found:
-                maiden_flights_oldFamily.append(mf_year)
-            else:
-                maiden_flights_newFamily.append(mf_year)
-                families_found.append(row["family"])
+        mf_year = PastT0s[PastRocket["configuration.id"] == row["id"]]["net"].min().year
+        if row["family"] in families_found:
+            maiden_flights_oldFamily.append(mf_year)
+        else:
+            maiden_flights_newFamily.append(mf_year)
+            families_found.append(row["family"])
     fig, axes = dark_figure()
     axes[0].hist([maiden_flights_newFamily, maiden_flights_oldFamily], bins=np.arange(1957, current_year + 2),
                  histtype='bar', stacked=True, label=["New rocket family", "Existing rocket family"],
