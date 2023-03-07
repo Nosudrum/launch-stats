3a95711250ee6060a2184a09f4000e006d97e59c
Nosudrum <arnaud.muller1308@gmail.com>
2023-03-07 15:10:29 +0100

    Added outcome indicators to yearly country plots

--- python/plotsCodes/YearlyOrbitalAttemptsPerCountry.py
+++ python/plotsCodes/YearlyOrbitalAttemptsPerCountry.py
@@ -1,8 +1,7 @@
 import calendar
 
 from tqdm import tqdm
-
-from Processing import PastT0s, PastCountries
+from Processing import PastT0s, PastCountries, PastStatus
 from plotsCodes.PlotFunctions import (
     Countries_dict,
     dark_figure,
@@ -18,18 +17,14 @@ from plotsCodes.PlotFunctions import (
 
 # Plot of orbital launch attempts per country per year since 1957
 def main(pbar, show=False):
-    F1y_README = open("plots/yearly/orbitalAttemptsPerCountry/README.md", "w")
-    F1y_README.write("# Orbital attempts per country for every year since 1957\n")
-    F1y_Countries_dict = Countries_dict.copy()
-    F1_Countries = PastCountries.copy()
-    F1_Countries_sorted = (
-        F1_Countries["location.country_code"].value_counts().index.tolist()
-    )
-    F1_Countries_selected = F1_Countries_sorted[0:7]
-    F1_Countries[
-        ~F1_Countries["location.country_code"].isin(F1_Countries_selected)
-    ] = "OTH"
-    F1_Countries_selected.append("OTH")
+    README = open("plots/yearly/orbitalAttemptsPerCountry/README.md", "w")
+    README.write("# Orbital attempts per country for every year since 1957\n")
+    Countries_dict_tmp = Countries_dict.copy()
+    Countries = PastCountries.copy()
+    Countries_sorted = Countries["location.country_code"].value_counts().index.tolist()
+    Countries_selected = Countries_sorted[0:7]
+    Countries[~Countries["location.country_code"].isin(Countries_selected)] = "OTH"
+    Countries_selected.append("OTH")
     for year in tqdm(
         range(1957, datetime.now(timezone.utc).year + 1),
         desc="Years",
@@ -37,7 +32,7 @@ def main(pbar, show=False):
         position=1,
         leave=False,
     ):
-        F1y_README.write(
+        README.write(
             "![Orbital attempts per country in "
             + str(year)
             + "]("
@@ -46,60 +41,124 @@ def main(pbar, show=False):
         )
         days = list(range(1, 1 + (366 if calendar.isleap(year) else 365)))
         if year > 1991:
-            F1y_Countries_dict["RUS"] = "Russia"
+            Countries_dict_tmp["RUS"] = "Russia"
         else:
-            F1y_Countries_dict["RUS"] = "USSR"
+            Countries_dict_tmp["RUS"] = "USSR"
         if year > 1957:
-            del F1y
-        F1y, F1y_axes = dark_figure()
-        F1y_T0s = PastT0s[PastT0s["net"].dt.year == year].copy()
-        F1y_Countries = (
+            del fig
+        fig, axes = dark_figure()
+        T0s = PastT0s[PastT0s["net"].dt.year == year].copy()
+        Countries_yearly = (
             PastCountries[PastT0s["net"].dt.year == year]["location.country_code"]
             .to_frame()
             .copy()
         )
-        F1y_Countries[
-            ~F1y_Countries["location.country_code"].isin(F1_Countries_selected)
+        Countries_yearly[
+            ~Countries_yearly["location.country_code"].isin(Countries_selected)
         ] = "OTH"
         if year == datetime.now(timezone.utc).year:
-            F1y_bins = np.arange(
+            bins = np.arange(
                 days[0], datetime.now(timezone.utc).timetuple().tm_yday + 2
             )
         else:
-            F1y_bins = np.append(days, max(days) + 1)
-        for ii in enumerate(F1_Countries_selected):
-            F1y_data_tmp = F1y_T0s[F1y_Countries["location.country_code"] == ii[1]][
-                "net"
-            ].dt.dayofyear.to_list()
-            if F1y_data_tmp:
-                count, edges = np.histogram(F1y_data_tmp, bins=F1y_bins)
-                F1y_axes[0].step(
+            bins = np.append(days, max(days) + 1)
+        legend_failure = False
+        legend_partial = False
+        for ii in enumerate(Countries_selected):
+            yearly_country_dayofyear = T0s[
+                Countries_yearly["location.country_code"] == ii[1]
+            ]["net"].dt.dayofyear.to_list()
+            yearly_country_failure = T0s.loc[
+                (Countries_yearly["location.country_code"] == ii[1])
+                & (PastStatus["id"] == 4)
+            ]["net"].dt.dayofyear.to_list()
+            yearly_country_partial = T0s.loc[
+                (Countries_yearly["location.country_code"] == ii[1])
+                & (PastStatus["id"] == 7)
+            ]["net"].dt.dayofyear.to_list()
+            if yearly_country_dayofyear:
+                count, edges = np.histogram(yearly_country_dayofyear, bins=bins)
+                cumulative_count = count.cumsum()
+
+                axes[0].step(
                     edges[:-1],
-                    count.cumsum(),
-                    linewidth=1.5,
+                    cumulative_count,
+                    linewidth=1.2,
                     color=colors[ii[0]],
-                    label=F1y_Countries_dict[ii[1]],
+                    label=Countries_dict_tmp[ii[1]],
                 )
+
+                index_failure = [
+                    np.where(edges == item)[0][0] for item in yearly_country_failure
+                ]
+                # [edges.index(item) for item in yearly_country_failure]
+                if index_failure:
+                    legend_failure = True
+                    edges_failure = edges[index_failure]
+                    count_failure = cumulative_count[index_failure]
+                    axes[0].scatter(
+                        edges_failure - 1,
+                        count_failure,
+                        marker="*",
+                        c=colors[ii[0]],
+                        s=20,
+                    )
+                index_partial = [
+                    np.where(edges == item)[0][0] for item in yearly_country_partial
+                ]
+                # [edges.index(item) for item in yearly_country_partial]
+                if index_partial:
+                    legend_partial = True
+                    edges_partial = edges[index_partial]
+                    count_partial = cumulative_count[index_partial]
+                    axes[0].scatter(
+                        edges_partial - 1,
+                        count_partial,
+                        marker="^",
+                        c=colors[ii[0]],
+                        s=12,
+                    )
+
+        if legend_failure:
+            axes[0].scatter(
+                [],
+                [],
+                marker="*",
+                c="white",
+                label="Failure",
+                edgecolors="none",
+            )
+        if legend_partial:
+            axes[0].scatter(
+                [],
+                [],
+                marker="^",
+                c="white",
+                label="Partial Failure",
+                edgecolors="none",
+            )
+
         handles, labels = prepare_legend(reverse=False)
-        F1y_axes[0].legend(
+        axes[0].legend(
             handles,
             labels,
             loc="upper center",
             ncol=4,
             frameon=False,
             labelcolor="white",
+            markerscale=1.4,
         )
-        F1y_axes[0].set_xticks(
+        axes[0].set_xticks(
             [datetime(year, i, 1).timetuple().tm_yday for i in range(1, 13)],
             monthsLabels,
         )
-        F1y_axes[0].set(
+        axes[0].set(
             ylabel="Cumulative number of launches",
             xlim=[1, max(days)],
             title="Orbital launch attempts per country in " + str(year),
         )
         finish_figure(
-            F1y, F1y_axes, "yearly/orbitalAttemptsPerCountry/" + str(year), show=show
+            fig, axes, "yearly/orbitalAttemptsPerCountry/" + str(year), show=show
         )
-    F1y_README.close()
+    README.close()
     pbar.update()
